42f075821df318b069378e3cc59b0758f90213a4
ChristianVillegas <christian.v.garcia@gmail.com>
2019-11-28 19:42:47 +0100

    MenuView refactoring

--- app/src/main/java/com/theopensourcefamily/mvp/MenuView.java
+++ app/src/main/java/com/theopensourcefamily/mvp/MenuView.java
@@ -31,14 +31,14 @@ public class MenuView extends AppCompatActivity {
         setContentView(R.layout.activity_menu_view);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-        FloatingActionButton fab = findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+        //FloatingActionButton fab = findViewById(R.id.fab);
+       /* fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
             }
-        });
+        });*/
         DrawerLayout drawer = findViewById(R.id.drawer_layout);
         NavigationView navigationView = findViewById(R.id.nav_view);
         // Passing each menu ID as a set of Ids because each
@@ -46,9 +46,14 @@ public class MenuView extends AppCompatActivity {
 
         //boolean isAdmin = user.role.equals("admin");
         //if (isAdmin)
-        mAppBarConfiguration = new AppBarConfiguration.Builder(
+        /*mAppBarConfiguration = new AppBarConfiguration.Builder(
                 R.id.nav_home, R.id.nav_gallery, R.id.nav_slideshow,
                 R.id.nav_tools, R.id.nav_share, R.id.nav_send)
+                .setDrawerLayout(drawer)
+                .build();*/
+
+        mAppBarConfiguration = new AppBarConfiguration.Builder(
+                R.id.nav_home, R.id.nav_gallery, R.id.nav_slideshow)
                 .setDrawerLayout(drawer)
                 .build();
 
