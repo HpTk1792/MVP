572dbce83352822b14d73ae7f33f2f6b0cd61426
cf18andres.lopez <andreslq1987.alq@gmail.com>
2019-12-02 18:56:35 +0100

    Class AccessData finished saveUsers and add methods getUsers,saveWorklogs,getWorklogs

--- app/src/main/java/com/theopensourcefamily/mvp/Models/AccessData.java
+++ app/src/main/java/com/theopensourcefamily/mvp/Models/AccessData.java
@@ -1,10 +1,13 @@
 package com.theopensourcefamily.mvp.Models;
 
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
+import java.util.ArrayList;
 
 /**
  *  Class AccessData manage the different methods to guard or load data
@@ -15,19 +18,80 @@ public class AccessData {
     private File UsersFile = new File("Users.bin");
 
     /**
-     * Method for save user into file of users
+     * Method for save users into file of users
      *
-     * @param user Object User
+     * @param users Object User
      */
-    public void SaveUser(User user){
+    public void saveUsers(ArrayList<User> users){
         try {
             ObjectOutputStream  oos = new ObjectOutputStream(new FileOutputStream(UsersFile));
-            oos.writeObject(user);
+            oos.writeObject(users);
             oos.close();
         } catch (FileNotFoundException e) {
             e.printStackTrace();
         } catch (IOException e) {
             e.printStackTrace();
         }
+    }
+
+    /**
+     * Method get all users on file users
+     *
+     * @return ArrayList<User>
+     */
+    public ArrayList<User> getUsers(){
+        ArrayList<User> Users = null;
+        try {
+            ObjectInputStream ois = new ObjectInputStream(new FileInputStream(UsersFile));
+            Users =  (ArrayList<User>) ois.readObject();
+            ois.close();
+        }  catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+        return Users;
+    }
+
+    /**
+     * Method for save the work log of user
+     *
+     * @param user User receives the work log
+     * @param worklogs ArrayList<Worklog> Array list whit all work logs
+     */
+    public void saveWorklogs(User user,ArrayList<Worklog> worklogs){
+        try {
+            ObjectOutputStream  oos = new ObjectOutputStream(new FileOutputStream(user.getUsername() + ".bin"));
+            oos.writeObject(worklogs);
+            oos.close();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * Method get all work logs of user
+     *
+     * @param user User user to want to get work logs
+     * @return ArrayList<Worklog> with all work logs
+     */
+    public ArrayList<Worklog> getWorklogs(User user){
+        ArrayList<Worklog> wlogs = null;
+        try {
+            ObjectInputStream ois = new ObjectInputStream(new FileInputStream(user.getUsername() + ".bin"));
+            wlogs =  (ArrayList<Worklog>) ois.readObject();
+            ois.close();
+        }  catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+        return wlogs;
     }
 }
