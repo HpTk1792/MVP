11272ba7b2704def4fe172f83c4ce0b6b91dd134
HpTk1792 <alejandroquirogasolis@gmail.com>
2019-12-02 19:21:36 +0100

    added the import in login fragment

--- app/src/main/java/com/theopensourcefamily/mvp/ui/login/LoginFragment.java
+++ app/src/main/java/com/theopensourcefamily/mvp/ui/login/LoginFragment.java
@@ -15,6 +15,7 @@ import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
 
+import com.theopensourcefamily.mvp.MenuView;
 import com.theopensourcefamily.mvp.R;
 
 public class LoginFragment extends Fragment {
