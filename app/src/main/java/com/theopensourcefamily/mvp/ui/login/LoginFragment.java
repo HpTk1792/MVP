b389334f1a1d18d421a768033e9cf89da5e4d16a
HpTk1792 <alejandroquirogasolis@gmail.com>
2019-12-03 19:17:58 +0100

    linked 2 fragments and some cleaning

--- app/src/main/java/com/theopensourcefamily/mvp/ui/login/LoginFragment.java
+++ app/src/main/java/com/theopensourcefamily/mvp/ui/login/LoginFragment.java
@@ -1,20 +1,16 @@
 package com.theopensourcefamily.mvp.ui.login;
 
 import androidx.lifecycle.ViewModelProviders;
-
 import android.content.Intent;
 import android.os.Bundle;
-
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
-
 import com.theopensourcefamily.mvp.MenuView;
 import com.theopensourcefamily.mvp.R;
 
